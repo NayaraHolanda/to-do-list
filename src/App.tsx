feed74b0df2a7f807e9efec9d9257786164c0367
Nayara Holanda <naayaraholanda@gmail.com>
2023-07-24 18:05:53 -0300

    Update main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import './App.module.css'
+import { ChangeEvent, useState } from 'react'
+import styles from './App.module.css'
+import logo from './assets/logo.svg'
+import plus from './assets/plus.svg'
 
 function App() {
-  return <h1>Vite + React</h1>
+  const [newTask, setNewTask] = useState('')
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value)
+  }
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img src={logo} className="logo" alt="ToDo Logotipo" />
+        <div className={styles.newTask}>
+          <input
+            type="text"
+            name=""
+            id=""
+            data-filled={newTask !== ''}
+            value={newTask}
+            placeholder="Adicione uma nova tarefa"
+            onChange={handleNewTaskChange}
+          />
+          <button type="button">
+            Criar
+            <img src={plus} alt="plus" width="16" height="16" />
+          </button>
+        </div>
+      </header>
+      <main>
+        <h1>Teste</h1>
+      </main>
+    </div>
+  )
 }
 
 export default App
